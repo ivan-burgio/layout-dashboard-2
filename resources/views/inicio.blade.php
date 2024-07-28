b0796f9e7f4f9ffc47bc7fa44ae17d8e5b677127
ivan-burgio <ivan.24.burgio@gmail.com>
2024-07-28 19:09:55 -0300

    Tailwind

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buyar</title>
+    @vite('resources/css/app.css')
+    <title>BUYAR</title>
 </head>
+
 <body>
-    hola
+    
 </body>
+
 </html>
