d26bbd6fc7fff9c6b13ff122e48bfc33f88408f8
ivan-burgio <ivan.24.burgio@gmail.com>
2024-07-30 09:29:39 -0300

    Mejora navbar

--- resources/views/templates/nav.blade.php
+++ resources/views/templates/nav.blade.php
@@ -1,14 +1,13 @@
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <img src="" class="h-8" alt="Buyar Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Buyar</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button"
             class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default" aria-expanded="false">
-            <i class="fa-solid fa-bars w-full text-2xl text-white cursor-pointer" aria-hidden="true" fill="none"
-                viewBox="0 0 17 14"></i>
+            <i class="fa-solid fa-bars w-full text-3xl text-white cursor-pointer" aria-hidden="true"></i>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul
