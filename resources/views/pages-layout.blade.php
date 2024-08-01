596c0ecca5715bb71b6628691e0319d39ce9f4a4
ivan-burgio <ivan.24.burgio@gmail.com>
2024-08-01 14:53:39 -0300

    footer simple

--- resources/views/pages-layout.blade.php
+++ resources/views/pages-layout.blade.php
@@ -14,6 +14,7 @@
 <body>
     @include('templates.nav')
     @yield('content')
+    @include('templates.footer')
 
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="bg-white shadow dark:bg-gray-900 p-2">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+
+        <div class="block text-sm text-gray-500 text-center dark:text-gray-400">© 2024
+            <a href="/" class="hover:underline">Buyar</a>. Todos los Derechos Reservados.
+        </div>
+    </div>
+</footer>
