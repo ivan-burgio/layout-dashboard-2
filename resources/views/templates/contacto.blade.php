427f9a39b8529971f6752d96603b825441649255
ivan-burgio <ivan.24.burgio@gmail.com>
2024-08-02 11:02:57 -0300

    Correccion de desbordamiento

--- resources/views/templates/contacto.blade.php
+++ resources/views/templates/contacto.blade.php
@@ -3,43 +3,43 @@
         <div class="lg:flex lg:items-center lg:-mx-6">
             <div class="lg:w-1/2 lg:mx-6">
                 <h1 class="text-2xl font-semibold text-gray-800 capitalize dark:text-white lg:text-3xl">
-                    Contactanos <br> para mas información
+                    Contáctanos <br> para más información
                 </h1>
 
                 <div class="mt-6 space-y-8 md:mt-8">
                     <p class="flex items-start -mx-2">
                         <i class="fa-solid fa-phone w-6 h-6 mx-2 text-blue-500 dark:text-blue-400 text-2xl"></i>
-                        <span class="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">(+598) 90 000 000</span>
+                        <span class="mx-2 text-gray-700 truncate dark:text-gray-400">(+598) 90 000 000</span>
                     </p>
 
                     <p class="flex items-start -mx-2">
                         <i class="fa-solid fa-envelope w-6 h-6 mx-2 text-blue-500 dark:text-blue-400 text-2xl"></i>
-                        <span class="mx-2 text-gray-700 truncate w-72 dark:text-gray-400">acb@example.com</span>
+                        <span class="mx-2 text-gray-700 truncate dark:text-gray-400">acb@example.com</span>
                     </p>
                 </div>
 
-                <div class="mt-6 w-80 md:mt-8">
-                    <h3 class="text-gray-600 dark:text-gray-300 ">Siguenos</h3>
+                <div class="mt-6 md:mt-8">
+                    <h3 class="text-gray-600 dark:text-gray-300 ">Síguenos</h3>
 
-                    <div class="flex mt-4 -mx-1.5 ">
+                    <div class="flex mt-4 -mx-1.5">
                         <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500 hover:cursor-pointer"
                             href="#">
-                            <i class="fa-brands fa-twitter w-10 h-10 fill-current text-2xl"></i>
+                            <i class="fa-brands fa-twitter text-2xl"></i>
                         </a>
 
                         <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500 hover:cursor-pointer"
                             href="#">
-                            <i class="fa-brands fa-linkedin-in w-10 h-10 fill-current text-2xl"></i>
+                            <i class="fa-brands fa-linkedin-in text-2xl"></i>
                         </a>
 
                         <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500 hover:cursor-pointer"
                             href="#">
-                            <i class="fa-brands fa-facebook-f w-10 h-10 fill-current text-2xl"></i>
+                            <i class="fa-brands fa-facebook-f text-2xl"></i>
                         </a>
 
                         <a class="mx-1.5 dark:hover:text-blue-400 text-gray-400 transition-colors duration-300 transform hover:text-blue-500 hover:cursor-pointer"
                             href="#">
-                            <i class="fa-brands fa-instagram w-10 h-10 fill-current text-2xl"></i>
+                            <i class="fa-brands fa-instagram text-2xl"></i>
                         </a>
                     </div>
                 </div>
@@ -47,8 +47,8 @@
 
             <div class="mt-8 lg:w-1/2 lg:mx-6">
                 <div
-                    class="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">
-                    <h1 class="text-lg font-medium text-white">¿Que quieres preguntarnos?</h1>
+                    class="w-full px-6 py-8 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">
+                    <h1 class="text-lg font-medium text-white">¿Qué quieres preguntarnos?</h1>
 
                     <form class="mt-6">
                         <div class="flex-1">
