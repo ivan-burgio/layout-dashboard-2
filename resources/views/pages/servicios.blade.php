5a1c517853f947a14b43ac8fbd84fded6b2d70c6
ivan-burgio <ivan.24.burgio@gmail.com>
2024-08-06 14:16:12 -0300

    Mejora visual en servicios

--- resources/views/pages/servicios.blade.php
+++ resources/views/pages/servicios.blade.php
@@ -1,13 +1,13 @@
 @extends('pages-layout')
 
 @section('content')
-    <section class="p-8 mt-10 bg-gray-100">
+    <section class="p-8 mt-10 bg-gray-100 flex flex-col items-center">
         <h2 class="text-4xl font-bold mb-8 text-center text-indigo-600">Nuestros Servicios</h2>
 
         @foreach ($data as $title => $service)
-            <div class="bg-white p-6 rounded-lg shadow-md mb-8">
-                <h3 class="text-3xl font-semibold mb-4 text-gray-800">{{ $title }}</h3>
-                <p class="text-base text-gray-700 leading-relaxed">
+            <div class="bg-white p-6 rounded-lg shadow-md mb-8 w-3/4">
+                <h3 class="text-3xl font-semibold mb-4 text-gray-800 px-12 py-8">{{ $title }}</h3>
+                <p class="text-base text-gray-700 leading-relaxed px-12 py-8">
                     {!! nl2br(e($service['description'])) !!}
                 </p>
 
