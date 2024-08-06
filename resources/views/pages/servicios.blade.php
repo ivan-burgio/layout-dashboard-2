fc73e109824371077b11121551f9e235e54095b7
ivan-burgio <ivan.24.burgio@gmail.com>
2024-08-06 17:23:20 -0300

    mejora en paddings en servicios

--- resources/views/pages/servicios.blade.php
+++ resources/views/pages/servicios.blade.php
@@ -5,9 +5,9 @@
         <h2 class="text-4xl font-bold mb-8 text-center text-indigo-600">Nuestros Servicios</h2>
 
         @foreach ($data as $title => $service)
-            <div class="bg-white p-6 rounded-lg shadow-md mb-8 w-3/4">
-                <h3 class="text-3xl font-semibold mb-4 text-gray-800 px-12 py-8">{{ $title }}</h3>
-                <p class="text-base text-gray-700 leading-relaxed px-12 py-8">
+            <div class="bg-white p-16 rounded-lg shadow-md mb-8 w-3/4">
+                <h3 class="text-3xl font-semibold mb-4 text-gray-800 pb-8">{{ $title }}</h3>
+                <p class="text-base text-gray-700 leading-relaxed pb-8">
                     {!! nl2br(e($service['description'])) !!}
                 </p>
 
