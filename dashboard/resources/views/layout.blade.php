d183d5e80256d0ddab33ce61e812c7b2abeeee8c
ivan-burgio <ivan.24.burgio@gmail.com>
2024-08-09 14:29:21 -0300

    tailwind al dashboard

---
+++ dashboard/resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Dashboard">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" />
+</head>
+
+<body class="bg-slate-200 text-black">
+    {{-- @include('templates.nav') --}}
+    @yield('content')
+    {{-- @include('templates.footer') --}}
+</body>
+
+</html>
